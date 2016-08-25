f9fddc754ceb0f3871efb85a3145bdd005c99131
Sami Emad <samiemad92@gmail.com>
2016-08-26 01:51:10 +0300

    fix: form validation, stay on the same page! favIcon bigger
    Signed-off-by: Sami Emad <samiemad92@gmail.com>

--- js/contact_me.js
+++ js/contact_me.js
@@ -21,7 +21,8 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+//                url: "././mail/contact_me.php",
+                url: "https://formspree.io/samiemad92@gmail.com",
                 type: "POST",
                 data: {
                     name: name,
@@ -30,31 +31,23 @@ $(function() {
                     message: message
                 },
                 cache: false,
-                success: function() {
-                    // Enable button & show success message
-                    $("#btnSubmit").attr("disabled", false);
-                    $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
-                    $('#success > .alert-success')
-                        .append('</div>');
-
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-                error: function() {
-                    // Fail message
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
-                    $('#success > .alert-danger').append('</div>');
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
+                success: AjaxSucceeded,
+                error: AjaxSucceeded,
             });
+			function AjaxSucceeded() {
+				// Enable button & show success message
+				$("#btnSubmit").attr("disabled", false);
+				$('#success').html("<div class='alert alert-success'>");
+				$('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+					.append("</button>");
+				$('#success > .alert-success')
+					.append("<strong>Your message has been sent. </strong>");
+				$('#success > .alert-success')
+					.append('</div>');
+
+				//clear all fields
+				$('#contactForm').trigger("reset");
+			}
         },
         filter: function() {
             return $(this).is(":visible");
